0c295f01f256ed67817b188d014cf8cdacf37747
Acha-Goodness <acharious1668@gmail.com>
2023-03-21 11:39:20 +0100

    Navigation implementation

---
+++ src/Component/CreateAccount/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./createAccount.css";
+
+const CreateAccount = () => {
+  return (
+    <div className="create_account">
+      <h1>CREATE ACCOUNT PAGE</h1>
+    </div>
+  )
+}
+
+export default CreateAccount
---
+++ src/Component/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./login.css";
+
+const Login = () => {
+  return (
+    <div classNama="login">
+      <h1>LOGIN PAGE</h1>
+    </div>
+  )
+}
+
+export default Login
