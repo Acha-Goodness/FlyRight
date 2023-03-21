3ace5466df6863f3565b7995dae23d2a7da51183
Kehinde <kennydaniels138@gmail.com>
2023-03-21 10:00:40 +0100

    Homepage Development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from "./Component/Header/Header";
+import Home from './Component/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 }
