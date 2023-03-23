5ed0d601868681978ef445d063ec3d69e1717b00
Kehinde <kennydaniels138@gmail.com>
2023-03-23 16:36:08 +0100

    login page development

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Component/Landing/Home';
 import Header from "./Component/Header/Header";
 import Login from "./Component/Login/Login";
 import CreateAccount from "./Component/CreateAccount/CreateAccount";
+import ForgotPassword from './Component/ForgotPassword/Forget';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/createAccount" element={<CreateAccount/>}/>
+            <Route path="/forgot" element={<ForgotPassword/>}/>
           </Routes>
       </Router>
     </div>
