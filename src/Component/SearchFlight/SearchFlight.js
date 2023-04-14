7eb1ab03dd0619fbdbcb5874c89b1352a6b3a44e
Bolaji Disu <abolajiidiisu@gmail.com>
2023-04-14 14:44:55 +0100

    fixed bugs on search flight booking

--- src/Component/SearchFlight/SearchFlight.js
+++ src/Component/SearchFlight/SearchFlight.js
@@ -3,6 +3,7 @@ import "./searchFlight.css";
 import { useLocation } from 'react-router-dom';
 import ViewFlightDetails from './ViewFlightDetails/ViewFlightDetails';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const SearchFlight = () => {
   const location = useLocation();
@@ -11,30 +12,71 @@ const SearchFlight = () => {
 useEffect(() => {
     setDetails(location.state.flightData);
 },[])
-  // const [availableFlight] = useGet("http://localhost:8000/flight");
-
-  // let flightDetails;
-
-  //   if(location.state !== null){
-  //     flightDetails = {
-  //       from:location.state.booking.from,
-  //       to:location.state.booking.to,
-  //       departureDateTime:location.state.booking.departureDateTime,
-  //       returnDateTime:location.state.booking.returnDateTime,
-  //       flightCategory:location.state.booking.flightCategory,
-  //       adult:location.state.booking.adult,
-  //       children:location.state.booking.children,
-  //       infants:location.state.booking.infants
-  //     }
-  //     // for(var x = 0; x <= 10; x++){
-  //     //   console.log(location.state.airLines)
-  //     //   setAvailableFlight(location.state.airLines)
-  //     // }
-  //   }
        
   const [ showEdit, setShowEdit ] = useState(false);
   const [ showAirView, setShowAirVies ] = useState(false);
   const [ airLineId, setAirLineId ] = useState(null);
+  const [airCodes, setAirCodes] = useState([]);
+
+   const options = {
+     method: "GET",
+     url: "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/airlines.json",
+     headers: {
+       "X-Access-Token": "158bac6b46f73503986540b6e022b2ae",
+       "X-RapidAPI-Key": "d834321b38msh0cdad8e30508789p119f2ajsn9ea9e60f7cd3",
+       "X-RapidAPI-Host":
+         "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+     },
+   };
+
+    const changeAirLineName = (data) => {
+      console.log("hihihi");
+      // console.log(airCodes[9].code);
+      for (let i =0; i < airCodes.length; i++){
+        if (airCodes[i].code === data){
+              console.log("consoling air line code");
+              console.log(airCodes[i]);
+        }
+      }
+      // for (let key in airCodes) {
+      //   if (airCodes[key].code === details.main_airline) {
+      //     console.log("consoling air line code");
+      //     console(airCodes[key]);
+      //   }
+      // }
+    };
+
+    const getAllAirlineCodes = async () => {
+      try {
+        let req = await axios.request(options);
+        let data = req.data
+        console.log("loggging data");
+        setAirCodes(data)
+        console.log(airCodes);
+      }catch(err){
+        console.error(err);
+      }
+  //   await axios
+  //  .request(options)
+  //  .then(function (response) {
+  //    setAirCodes(response.data);
+  //    console.log("consoling airline data");
+  //    console.log(airCodes);
+  //      changeAirLineName("VS");
+  //  })
+  //  .catch(function (error) {
+  //    console.error(error);
+  //  });
+    }
+
+   useEffect(() => {
+    getAllAirlineCodes();
+
+   }, []);
+
+  
+    
+   
 
   const handleEdit = () => {
     setShowEdit(!showEdit)
@@ -45,13 +87,20 @@ useEffect(() => {
   }
 
   const toggleAirView = (id) => {
-    setAirLineId(id)
-    setShowAirVies(true);
+    // console.log(details);
+    setAirLineId(details[id])
+      setShowAirVies(true);
   }
 
   const closeAirView = (id) => {
     setShowAirVies(false);
   }
+
+  const formatDate = (isoDate) => {
+    const date = new Date(isoDate);
+    return date.toLocaleDateString();
+  };
+
   return (
     <div className="search_flight">
       {/* <div className="search_flight_select">
@@ -102,21 +151,25 @@ useEffect(() => {
        </div> */}
       <div className="air">
         <div className="air_available_flight">
-          {details.map((data, index) => (
+          {details.map((data, id) => (
             <div className="air_lines">
-              <p>{data.main_airline}</p>
               <div>
-                <p>{data.depart_date}</p>
-                <p>{data.origin}</p>
-                <div>
-                  <p>{data.found_at}</p>
-                  <p>{data.destination}</p>
-                </div>
-                <div className="air_line_price">
-                  <p>N {data.price * 760}.00</p>
-                  <button onClick={() => toggleAirView(data.index)}>View</button>
-                </div>
+                <p>{(data.main_airline)}</p>
               </div>
+              <div>
+                <p>Depart Time</p>
+                <p>{formatDate(data.found_at)}</p>
+                <p>{data.destination}</p>
+              </div>
+              <div>
+                <p>Arrival Date</p>
+                <p>{formatDate(data.depart_date)}</p>
+                <p>{data.origin}</p>
+              </div>
+              <div className="air_line_price">
+                <p>N {data.price * 700}.00</p>
+              </div>
+              <button onClick={() => toggleAirView(id)}>View</button>
             </div>
           ))}
 
@@ -139,11 +192,11 @@ useEffect(() => {
           })} */}
         </div>
         <div className={showAirView ? "air_flight_details" : "hideAirDetails"}>
-          <ViewFlightDetails airId={airLineId} closeView={closeAirView} />
+          <ViewFlightDetails airFlight={airLineId} closeView={closeAirView} />
         </div>
       </div>
     </div>
   );
-}
 
+}
 export default SearchFlight;
--- src/Component/SearchFlight/ViewFlightDetails/ViewFlightDetails.js
+++ src/Component/SearchFlight/ViewFlightDetails/ViewFlightDetails.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import "./viewFlightDetails.css";
 import { useGet } from '../../../Utils/Hooks';
+import axios from "axios";
+import { useEffect } from 'react';
 
-const ViewFlightDetails = ({airId, closeView}) => {
-    const [ airFlight ] = useGet(`http://localhost:8000/flight/${airId}`);
+const ViewFlightDetails = ({airFlight, closeView}) => {
+    // const [ airFlight ] = useGet(`http://localhost:8000/flight/${airId}`);
+
+   
+
+    
 
 
     return (
-    <div className="vFlight">
+      <div className="vFlight">
         <div className="vflight_header">
-            <h3>Air Flight Breakdown</h3>
-            <button className="XxBtn" onClick={closeView}><p>&#x274C;</p></button>
+          <h3>Air Flight Breakdown</h3>
+          <button className="XxBtn" onClick={closeView}>
+            <p>&#x274C;</p>
+          </button>
         </div>
         <div>
-           <div className="fare_total"><h4>Total</h4><p>{airFlight && airFlight.Price}</p></div>
+          <div className="fare_total">
+            <h4>Total</h4>
+            <p>N {airFlight && airFlight.price * 700}.00</p>
+          </div>
         </div>
         <div className="air_note">
-            <h4>Air Line Info</h4>
-            <p>{airFlight && airFlight.note}</p>
+          <h4>AirLine Name:</h4>
+          <p> {airFlight && airFlight.main_airline} Airline</p>
         </div>
         <div className="air_details">
-            <h4>Departure Flight:</h4>
-            <p>{airFlight && airFlight.Name}</p>
-            <div className="air_flight_flow">
-                <div className="flight_origin">
-                    <p>{airFlight && airFlight.FromCity}</p>
-                    <p>{airFlight && airFlight.IataOrig} - {airFlight && airFlight.DepartTime}</p>
-                    <p>{airFlight && airFlight.DepartDate}</p>
-                </div>
-                <div className="flight_origin">
-                    <p>{airFlight && airFlight.ToCity}</p>
-                    <p>{airFlight && airFlight.IataDest} - {airFlight && airFlight.ArriveTime}</p>
-                    <p>{airFlight && airFlight.ArriveDate}</p>
-                </div>
-            </div>
+          <h4>Flight Duration: </h4>
+          <p>{airFlight && Math.round(airFlight.duration / 60)} hrs</p>
         </div>
-        <button className="book_now">BOOK NOW</button>
-    </div>
-  )
+        <button className="book_now">
+          <a href="/info">BOOK NOW</a>
+        </button>
+      </div>
+    );
 }
 
 export default ViewFlightDetails;
